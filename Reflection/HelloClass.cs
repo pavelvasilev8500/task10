aa908a73eb6eb62fd8c92d5735443614aa9b9e1b
pavelvasilev8500 <61418223+pavelvasilev8500@users.noreply.github.com>
2021-02-28 15:32:33 +0300

    Add files via upload

---
+++ Reflection/HelloClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    class HelloClass
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var typeInfo = new TypeInfo();
+            typeInfo.Class();
+        }
+    }
+}
